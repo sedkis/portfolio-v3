2aeda3912215c6bd6f0dbff58892abddc8f44d81
Sedky <sedkyaboushamalah@gmail.com>
2021-11-19 12:37:55 -0500

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
@@ -11,6 +15,23 @@ import { ClientReload } from '@/components/ClientReload'
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider attribute="class">
       <Head>
