4a56db2c5840fec0e22984466ec2424fb570b894
Sedky <sedkyaboushamalah@gmail.com>
2021-12-07 12:01:19 -0500

    Update URL

--- data/projectsData.js
+++ data/projectsData.js
@@ -16,7 +16,7 @@ const projectsData = [
     title: 'Founder @ SedKodes',
     description: `Teaching the world about software!`,
     imgSrc: '/static/images/projects/youtubelogo2.png',
-    href: '/blog/the-time-machine',
+    href: 'https://www.youtube.com/channel/UCXR-k7wwwhdovpXXkRitJ_g',
   },
   {
     title: 'Founder @ Ajtima',
